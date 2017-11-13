2d9af0540bc6b3e77a6c8f7279b695e69ec841cf
Jose <jrbass00@gmail.com>
2017-11-13 22:26:38 +0000

    initial files

---
+++ itsupport.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title> IT Support </title>
+</head>
+<body>
+<?php 
+    echo '<p> Web page for IT Support Database </p>'; 
+    $db_connection = pg_connect("host=localhost dbname=itsupport
+user=itsupport password=jrdd3420") or die('Failed to connect'); 
+$query = 'SELECT * FROM client';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+echo "<table>\n";
+    echo "\t<tr>\n";
+    for ($i = 0; $i <= 11; $i++) {
+    $fieldname = pg_field_name($result, $i);
+    echo "\t\t<th>$fieldname</th>";
+    }
+    echo "\t</tr>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+pg_free_result($result);
+pg_close($db_connection);
+
+?>
+</body>
+</html>
+
