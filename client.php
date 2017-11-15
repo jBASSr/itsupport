c01eb810df0917f1ce1116d28bc6aefe6f3a751c
dirk <you@example.com>
2017-11-15 22:03:27 +0000

    testing

--- client.php
+++ client.php
@@ -38,6 +38,12 @@ pg_free_result($result);
 </head>
 <body>
 
+<form action="welcome_get.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
 <?
 // ---------- FUNCTIONS ---------- //
 
