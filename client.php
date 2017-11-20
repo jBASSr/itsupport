225b9029d523e0fa0d13d18e8456c60ddc9ea87d
dirk <you@example.com>
2017-11-20 22:01:38 +0000

    testing

--- client.php
+++ client.php
@@ -109,7 +109,7 @@ Fax: <input type="text" name="fax">
 Email: <input type="text" name="email"><br><br>
 Problem Description:
 <br> 
-<textarea name="problem" maxlength = 150 id = problem_id style = "height: 100px; width: 300px;"></textarea><br><br>
+<form name="problem" maxlength = 150 id = problem_id style = "height: 100px; width: 300px;"></form><br><br>
 
 <button input type="submit" name="dbinsert">Submit</button>
 
