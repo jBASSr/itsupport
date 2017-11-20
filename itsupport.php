cc9b657d654fd7a211c4977a776b3a29bce4fb6e
jBASSr <jrbass00@gmail.com>
2017-11-20 21:20:16 +0000

    New table system

--- itsupport.php
+++ itsupport.php
@@ -1,41 +1,48 @@
 <html>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
-<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-<link rel="stylesheet" href="css/custom.css">
 <head>
-<title> IT Support Example </title>
+	<title> IT Support Example </title>
+	<link rel="stylesheet" href="/~jose/itsupport/css/table.css">
 </head>
 <body>
-<?php 
-    echo "<p> Web page for Employees for IT Support Database </p>"; 
-$db_connection = pg_connect("host=localhost dbname=itsupport user=itsupport password=jrdd3420");
-$query = 'SELECT * FROM client';
+	<p> Web page for Employees for IT Support Database </p>
+<table>
+<?php
+/*
+$db_connection = pg_connect("host=localhost dbname=itsupport user=itsupport
+password=jrdd3420");
+*/
+include("db.php");
+$query = 'SELECT * FROM employee ORDER BY employee_id';
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
-	echo "<div class='container'>\n";
-	echo "<table border='1'>\n";
-    echo "\t<tr>\n";
-    for ($i = 0; $i <= 11; $i++) {
+$i = 0;
+//<!-- TABLE -->
+while ($i < pg_num_fields($result)) {
     	$fieldname = pg_field_name($result, $i);
     	echo "\t\t<th>$fieldname</th>";
+    	$i = $i + 1;
     }
-    echo "\t</tr>\n";
-	while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-    	echo "\t<tr>\n";
-    	foreach ($line as $col_value) {
-        	echo "\t\t<td>$col_value</td>\n";
-    	}
-    	echo "\t</tr>\n";
-	}
-	echo "</table>\n";
-	echo "</div>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_NUM))
+{
+	$id = $line[0];
+	//$col = $line[1];
+	?>
+	<tr id="<?php echo $id; ?>" class="edit_tr">
+		<?php foreach ($line as $col) { ?>
+		<td id="edit_td">
+			<span id="first_<?php echo $id; ?>" class="test"><?php echo $col; ?></span>
+			<input type="text" value="<?php echo $col; ?>" class="editbox" id="last_input_<?php echo $id; ?>"/>
+		</td>
+		<?php } ?>
+	</tr>
+
+<?php
+}
 pg_free_result($result);
 pg_close($db_connection);
-
 ?>
+</table>
 </body>
-<!-- js -->
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js" ></script>
-<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.16/datatables.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/
+ajax/libs/jquery/1.5/jquery.min.js"></script>
 </html>
 
